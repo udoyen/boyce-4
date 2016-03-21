f63f4c7e24d4ed753e94b01b5a9be742367f42b0
udoyen <datameshprojects@gmail.com>
2016-03-21 21:42:15 +0100

    initial commit

---
+++ assets/js/genbrook.js
@@ -0,0 +1,9 @@
+/*$(document).ready(function(){
+   menuH = $('.navbar').outerHeight();
+   winH = window.innerHeight();
+   
+        $('.img-1').css({
+           'width':winH - menuH + '!important'
+        });
+    
+});*/
