78259ac4a326c8cea721f9eb06a4f27e7bbf41cd
udoyen <datameshprojects@gmail.com>
2016-03-22 15:47:15 +0100

    menu commit

--- assets/js/genbrook.js
+++ assets/js/genbrook.js
@@ -1,9 +1,51 @@
-/*$(document).ready(function(){
-   menuH = $('.navbar').outerHeight();
-   winH = window.innerHeight();
-   
-        $('.img-1').css({
-           'width':winH - menuH + '!important'
-        });
-    
-});*/
+$(document).ready(function () {
+    /*$('.my-click').popover({
+     title: "<h1><strong>HTML</strong> inside <code>the</code> <em>popover</em></h1>", 
+     content: "Blabla <br> <h2>Cool stuff!</h2>",
+     html: true, 
+     placement: "bottom",
+     trigger:'hover',
+     delay: {show:200, hide:2000}
+     });
+
+     $('.my-click').popover({
+     title: "<h1><strong>HTML</strong> inside <code>the</code> <em>popover</em></h1>", 
+     content: "Blabla <br> <h2>Cool stuff!</h2>",
+     html: true, 
+     placement: "bottom",
+     trigger:'focus',
+     delay: {show:200, hide:2000}
+     });*/
+
+    // get width of window
+    var w = $('#navcol-top').width();
+
+    $('.my-click').on({
+        mouseenter: function () {
+            if (w > 425) {
+                $('ul#nav-wrapper').toggleClass("appear");
+            } else {
+                alert('wrong');
+
+            }
+
+        },
+        hover: function () {
+            if (w > 425) {
+                $('ul#nav-wrapper').toggleClass("appear");
+            } else {
+                alert('wrong');
+
+            }
+        },
+        focus: function () {
+            if (w > 425) {
+                $('ul#nav-wrapper').toggleClass("appear");
+            } else {
+                alert('wrong');
+
+            }
+        }
+    })
+
+});
